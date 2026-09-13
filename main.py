9dcaf6cbafdfabe7fa8c908779bb67ad569450e9
mdhabeebvulla <55741151+mdhabeebvulla@users.noreply.github.com>
2026-09-13 10:53:10 +0530

    Implement FastMCP server for reversing words
    Added a FastMCP server with a reverse word tool.

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import os
+from mcp.server.fastmcp import FastMCP
+port = int(os.environ.get("PORT",10000))
 
+mcp = FastMCP(
+          "Reversewordserver",
+          host = "0.0.0.0",
+          port = port,
+          stateless_http = True,
+          json_response=True
+)
+@mcp.tool(description="Reverse a single word .Example : 'habeeb' -> 'beebah'")
+def reverse_word(word:str) ->str:
+  """Reverse a single word."""
+  return word[::-1]
+if __name__ == "__main__":
+  mcp.run(transport="streamable-http")
